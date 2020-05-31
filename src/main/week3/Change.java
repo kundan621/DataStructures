56b8603c2d639c645d736958a24649a911f2ec90
kundan621 <kundan621@gmail.com>
2020-05-31 12:55:27 +0530

    coursera

--- src/main/week3/Change.java
+++ src/main/week3/Change.java
@@ -1,11 +1,25 @@
-package main.week3;
 
 import java.util.Scanner;
 
 public class Change {
     private static int getChange(int m) {
-        //write your code here
-        return m;
+      int count = 0,rem=0;
+      if(m>=10) {
+          count = m / 10;
+          rem = m % 10;
+              count=count+rem/5;
+              rem=rem%5;
+              count=count+rem;
+      }
+     else  if(m>=5){
+          count= count+m/5;
+          rem=m%5;
+          count=count+rem;
+      }
+     else{
+         count=m;
+      }
+        return count;
     }
 
     public static void main(String[] args) {
